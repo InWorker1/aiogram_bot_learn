9cfc8c1a5b35dbb2c107e08add2ec2c0c4102204
InVorker <120738638+InWorker1@users.noreply.github.com>
2024-11-08 23:57:50 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -2,15 +2,18 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
-main_R = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='каталог')],
-    [KeyboardButton(text='корзина'), KeyboardButton(text='контакты')]
-],          resize_keyboard=True, input_field_placeholder='pick something')             
+# main_R = ReplyKeyboardMarkup(keyboard=[
+#     [KeyboardButton(text='каталог')],
+#     [KeyboardButton(text='корзина'), KeyboardButton(text='контакты')]
+# ],          resize_keyboard=True, input_field_placeholder='pick something')             
 # ----- создана клавиатура, в которой три кнопки. одна сверху и две снизу. ( Reply Buttons )
 # ----- 3 аргумента. первый отвечает за саму клавиатуру. второй за размер ( сводит размер к минимуму, так красивее) и 3 арг добавляет надпись к пользовательской клавиатуре
 
 main_I = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='my soundcloud', url='https://on.soundcloud.com/QcpGxm6GtvpDvPfE6')]])
 
+main_K = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='какие выпустились треки?', callback_data='give_how_much_traks')]])
+
+troll = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='show your traks', callback_data='troll')]])    # для прикола
 
 # cars = ['shine', 'emptiness', 'cheer up']                   # ----- пример, если названия меняются, а переписывать код не хочется. клавиатуру можно сделать настраиваемой под базу данных.                          
 # async def reply_cars():
