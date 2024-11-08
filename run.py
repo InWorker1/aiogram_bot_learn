9b590f6bad26f4305c2c379014a814d3550554be
InVorker <120738638+InWorker1@users.noreply.github.com>
2024-11-08 20:29:44 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,46 +1,17 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
-from aiogram.filters import CommandStart, Command
-# from aiogram import CommandStart
+from aiogram import Bot, Dispatcher
 from config import TOKEN
+from app.handlers import router
+
 
 bot = Bot(token=TOKEN)                  # ----- токен бота ( пиздец удивительно )
 dp = Dispatcher()                       # ----- диспетчер бота ( все что выполняется, выполняется через эту суку )
 
 
-@dp.message(CommandStart())             # ---- команда старта 
-async def cmd_start(msg: Message):
-    await msg.answer('hi! my name is von. i am music producer')
-    await msg.answer_photo(photo='AgACAgIAAxkBAAMuZy0bETXMO67UHV0Fy6cZ4U5iZNcAAsfnMRtg2HBJ3Am7vZLQFbkBAAMCAANtAAM2BA', caption='hi! my name is von. i am music producer')        # ---- команда, чтобы бот отрпавил фото (caption= - это текст, который будет вместе с фото отправлен)
-
-
-
-@dp.message(Command('ahelp'))           # ----- пример команды 
-async def get_help(msg: Message):
-    await msg.answer('в этом телеграмм боте вы можете: ')
-
-
-@dp.message(F.text.lower().strip() == 'как дела?') # ---- ответ на текстовое сообещение
-async def how_are_u(msg: Message):
-    await msg.answer('все в порядке!)')
-
-
-@dp.message(F.photo)                    # ----- работа c Ответом на фото 
-async def get_photo(msg: Message):
-    await msg.answer(f'ID photo: {msg.photo[-1].file_id}')              # ----- индекс минус один, потому что это лучшее качество фото в телеграмме
-
-
-@dp.message(Command('getID'))            # ----- команда дающая айди и имя пользователя, который ее использовал
-async def get_ID(msg: Message):
-    await msg.reply(f'your ID: {msg.from_user.id}\nyour name: {msg.from_user.first_name}') # ---- так же можно доаставать куча инфы с msg.from_user.
-                                                                                           # ----- reply делает так, что бот ОТВЕЧАЕТ на твое сообщение с командой ( и не только )
-
-
-
 async def main():       # ---- старт бота ( дефолт )
+    dp.include_router(router)       # ----- для того чтобы питон понимал, что роутер это диспетчер. 
     await dp.start_polling(bot) 
 
 if __name__ == '__main__':      # ----- всегда писать тоже дефолт ( такой же старт )
